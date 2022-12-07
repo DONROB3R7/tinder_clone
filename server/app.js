31318c2b74bc63dfef3ded627ab20c86224baf3f
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-12-07 16:14:50 +0100

    Updated

---
+++ server/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = 3001;
+const cors = require('cors')
+const app = express()
+
+
+require('dotenv').config()
+const dataUrl = process.env.MONGODB_URI
+
+
+// Middleware 
+
+app.use(cors())
+app.use(express.json())
+
+
+// Models 
+const User = require('./models/User.model');
+
+
+// DataBase Connected 
+const dataBase = dataUrl;
+mongoose.connect(dataBase, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    (result) => console.log('DataBase Connected')
+).catch((error)=> console.log(error));
+
+
+
+mongoose.set('strictQuery', false);
+
+
+app.post('/signup' ,(req, res) => {
+    // Getting the user from body
+    const password = req.body.password;
+    const email = req.body.email;
+
+    User.create({
+        email: email,
+        password: password,
+      })
+        .then((data) => {
+            console.log(data);
+            res.status(200).json('Token');
+        })
+        .catch((err) => {
+            console.log(err);
+        }); 
+})
+
+
+app.listen(PORT, () => console.log('server running on PORT ' + PORT))
+
+
