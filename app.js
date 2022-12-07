41f07da7e5bcff6a688b246b9ed5cbf2b30f1538
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-12-07 16:53:52 +0100

    New Strucutre

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ const cors = require('cors')
 const app = express()
 
 
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+
+
 require('dotenv').config()
 const dataUrl = process.env.MONGODB_URI
 
+console.log(dataUrl);
 
 // Middleware 
 
