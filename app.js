9ea96afa76143675a5d3ca638fb0582f9914aa1d
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-12-07 17:10:09 +0100

    App Updated

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const app = express()
 // Accessing the path module
 const path = require("path");
 
-// Step 1:
+
 app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
+
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
