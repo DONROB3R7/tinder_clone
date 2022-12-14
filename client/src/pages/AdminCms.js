bbf3d8d2d443543de16b618e0677a485fab17e97
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-12-14 15:28:38 +0100

    new updated

--- client/src/pages/AdminCms.js
+++ client/src/pages/AdminCms.js
@@ -14,7 +14,7 @@ export default function AdminCms() {
   const [Loading, setLoading] = useState(false);
 
     useEffect(() => {
-        fetch("http://localhost:3001/admincms")
+        fetch("/admincms")
         .then((res) => res.json())
         .then((data) => {
             setUsersData(data);
