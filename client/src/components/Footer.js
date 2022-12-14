547a089c35b8d6b686ed460649923625a03aa00f
Costache Robert Valentin <costache.robert.valentin@gmail.com>
2022-12-14 15:40:33 +0100

    Add

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
                 <li><Link to="/">Home Page</Link></li>
                 <li><Link to="/dashboard">Dashboard</Link></li>
                 <li><Link to="/onboarding">Onboarding</Link></li>
-                <li><Link to="/users">Users</Link></li>
+                <li><Link to="/users">Userss</Link></li>
                 <li><Link to="/admin-cms">Admin-cms</Link></li>
              </ul>
         </footer>
